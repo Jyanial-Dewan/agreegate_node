8c6c9a1d60b6668503a9b0b40ecb9bef3c81ae6f
kyaching <kyochingmarma@gmail.com>
2025-09-19 21:32:03 +0600

    add routes

--- Routes/defCombinedUsersRoutes.js
+++ Routes/defCombinedUsersRoutes.js
@@ -5,5 +5,6 @@ const router = Router();
 
 router.post("/register", defCombinedUsersController.createCombinedUser);
 router.get("/:userId", defCombinedUsersController.getSingleCombinedUser);
+router.put("/:userId", defCombinedUsersController.updateCombinedUser);
 
 module.exports = router;
