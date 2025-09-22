dae3bc1b72da33f68dd8cfb53a0e526fcef56793
nepolionchakma <nepolionchakma.nc@gmail.com>
2025-09-22 10:58:31 +0600

    rename authuser

--- Routes/authMeRoutes.js
+++
@@ -1,8 +0,0 @@
-const Router = require("express");
-const authMeController = require("../Controller/authMeController");
-
-const router = Router();
-
-router.get("/", authMeController.user);
-
-module.exports = router;
