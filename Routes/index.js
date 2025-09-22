107a690fd86cf9594439e6f41696d0e6b1fe81a1
Jyanial Dewan <djyanial@gmail.com>
2025-09-22 14:10:08 +0600

    ip address

--- Routes/index.js
+++ Routes/index.js
@@ -3,12 +3,14 @@ const defUsersRoutes = require("./defUsersRoutes");
 const defCombinedUsersRoutes = require("./defCombinedUsersRoutes");
 const authentication = require("./authenticationRoutes");
 const authUserRoutes = require("./authUserRoutes");
+const ipRoutes = require("./ipRoutes");
 const verifyUser = require("../Middleware/verifyUser");
 
 const routes = Router();
 routes.use("/api/auth", authentication);
 routes.use("/api/users", defUsersRoutes);
 routes.use("/api/combined_users", defCombinedUsersRoutes);
+routes.use("/api/geo", ipRoutes);
 routes.use(verifyUser);
 
 routes.use("/api/auth/verify_user", authUserRoutes);
